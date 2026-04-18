096b336368c7b210154ce6566a0533d9b21e2060
tina20k <tina.onlineorders@gmail.com>
2026-04-18 17:02:11 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'pmbia-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const CACHE_NAME = 'pmbia-v2';
+const ASSETS = ['/', '/index.html', '/manifest.json', '/apple-touch-icon.png', '/apple-touch-icon-180.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
